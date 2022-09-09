fb18ec29a7c0e326e317816a0491f83c958fcc87
athavan94 <athavan94@gmail.com>
2022-09-09 17:28:33 +0200

    First commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Webdownloader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test");
         }
     }
 }
