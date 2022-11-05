a52712fc9787b93810cd884e9bfd20d8c40604b9
athavan94 <athavan94@gmail.com>
2022-11-05 18:46:35 +0100

    CheckUrl()

--- Controller/Webpage.cs
+++ Controller/Webpage.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Webdownloader
 {
@@ -10,9 +11,18 @@ namespace Webdownloader
         List<Webpage> webpages;
         List<Content> contents;
 
-        public void CheckURL()
+        public bool CheckURL(string url)
         {
+            string pattern = @"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$";
+            Regex rgx = new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            
+            if (!rgx.IsMatch(url))
+            {
+                Console.WriteLine("URL is not valid. It should look be like this: http://urlname.ch");
+                return false;
+            }
 
+            return true;
         }
 
         public void Download()
