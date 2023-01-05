c97b88e5d0f7f692631b3f1d2870ca0fcb4cf21b
athavan94 <athavan94@gmail.com>
2023-01-05 16:13:03 +0100

    Create Crawler and FileWrite Class

---
+++ Crawler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Webdownloader
+{
+    class Crawler
+    {
+        private readonly string url;
+        private string htmlStr;
+
+        public Crawler(string url)
+        {
+            this.url = url;
+        }
+
+        public string GetHtmlResponseString()
+        {
+            try
+            {
+                WebRequest webRequest = WebRequest.Create(url);
+                webRequest.Proxy = null;
+
+                WebResponse webResponse = webRequest.GetResponse();
+
+                StreamReader streamReader = new StreamReader(webResponse.GetResponseStream());
+                htmlStr = streamReader.ReadToEnd();
+            }
+            catch (WebException ex)
+            {
+                throw ex;
+            }
+
+            return htmlStr;
+        }
+    }
+}
---
+++ FileWriter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Webdownloader
+{
+    class FileWriter
+    {
+        private string path;
+        private string url;
+        private readonly string htmlStr;
+
+        public FileWriter(string url, string htmlStr)
+        {
+            this.url = url;
+            this.htmlStr = htmlStr;
+
+            ReplaceUrl();
+        }
+
+        public void CreateFile()
+        {
+            path = Directory.GetCurrentDirectory() + "\\Test";
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string newPath = Path.Combine(path, url + ".txt");
+
+            using StreamWriter streamWriter = File.CreateText(newPath);
+
+            streamWriter.WriteLine(htmlStr);
+        }
+
+        private void ReplaceUrl()
+        {
+            string pattern = @"^(https?://)";
+            string replacement = "";
+            url = Regex.Replace(url, pattern, replacement);
+        }
+    }
+}
