b90110dbb102c8705c552150c7efe1a289d03ec0
athavan94 <athavan94@gmail.com>
2023-01-05 16:13:11 +0100

    Remove old classes

--- Program.cs
+++ Program.cs
@@ -1,20 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Webdownloader
 {
     class Program
     {
-        private static Seed seed;
-        private static Queue queue;
-        private static Crawled crawled;
+        private static Crawler crawler;
+        private static FileWriter fileWriter;
 
         static void Main(string[] args)
         {
-            StartCrawlerAsync();
+            //StartCrawlerAsync();
 
+
+            crawler = new Crawler(args[0]);
+            string htmlStr = crawler.GetHtmlResponseString();
+
+            fileWriter = new FileWriter(args[0], htmlStr);
+            fileWriter.CreateFile();
 
             Console.WriteLine("Drücken Sie eine beliebige Taste, um fortzufahren...");
             Console.ReadKey();
@@ -22,65 +28,6 @@ namespace Webdownloader
             //Download one page
             //Save somewhere
             //Save url from Webpage
-        }
-
-        private static async Task StartCrawlerAsync()
-        {
-            Initialize();
-
-            await Crawl();
-        }
-
-        private static async Task Crawl()
-        {
-            do
-            {
-                string url = queue.Top;
-
-                Crawl crawl = new Crawl(url);
-                await crawl.Start();
-
-                if (crawl.parsedURLs.Count > 0)
-                    await ProcessURLs(crawl.parsedURLs);
-
-                await PostCrawl(url);
-
-            } while (queue.HasURLs);
-        }
-
-        private static void Initialize()
-        {
-            string path = Directory.GetCurrentDirectory() + "\\Test";
-
-            if(!Directory.Exists(path))
-            {
-                Directory.CreateDirectory(path);
-            }
-
-            string seedPath = Path.Combine(path, "Seed.txt");
-            string queuePath = Path.Combine(path, "Queue.txt");
-            string crawledPath = Path.Combine(path, "Crawled.txt");
-
-            seed = new Seed(seedPath);
-            var seedURLs = seed.Items;
-            queue = new Queue(queuePath, seedURLs);
-            crawled = new Crawled(crawledPath);
-        }
-
-        static async Task ProcessURLs(List<string> urls)
-        {
-            foreach (var url in urls)
-            {
-                if (!crawled.HasBeenCrawled(url) && !queue.IsInQueue(url))
-                    await queue.Add(url);
-            }
-        }
-
-        static async Task PostCrawl(string url)
-        {
-            await queue.Remove(url);
-
-            await crawled.Add(url);
         }
     }
 }
